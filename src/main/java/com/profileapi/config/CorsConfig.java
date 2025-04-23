19c4fcf686b10ea14ebd2fa46ba341a949c4247f
Ayeong Choi <cdd8816@gmail.com>
2025-04-23 14:42:19 +0900

    feat: cors 추가

---
+++ src/main/java/com/profileapi/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.profileapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://loviary.com")
+                .allowedOrigins("https://loviary.com")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
